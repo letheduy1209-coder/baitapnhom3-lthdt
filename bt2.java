7abcc9faeddfb9632b9f777f2e608760b05835a2
truongvanchien0608 <truongvanchien082006@gmail.com>
2025-09-17 08:35:22 +0700

    bt2

---
+++ bt2.java
@@ -0,0 +1,18 @@
+public class bt2 
+{
+     public static void main(String[] args) 
+     {
+        String ten = "Truong Van Chien";
+        int tuoi = 19;
+        double chieucao = 1.70;
+        String gioitinh = "Nam";
+        boolean thichlaptrinh = true;
+
+        System.out.println("thong tin ca nhan");
+        System.out.println("ten: "+ ten);
+        System.out.println("tuoi: "+ tuoi);
+        System.out.println("chieu cao: "+ chieucao + "m");
+        System.out.println("gioi tinh: "+ gioitinh);
+        System.out.println("Thich lap trinh: "+ thichlaptrinh);
+     }
+}
